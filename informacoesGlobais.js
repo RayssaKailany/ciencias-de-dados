d35d695c294532f44285553b0bd4fcc61f3c95c5
Rayssa Kailany <102701353+RayssaKailany@users.noreply.github.com>
2024-10-22 07:50:11 -0300

    ok

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -3,5 +3,9 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 console.log(url);
 
 async function vizualizarInformacoesGlobais() {
-
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
 }
+
+vizualizarInformacoesGlobais()
