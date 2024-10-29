e9f8376a5195fec5948b7e18fe7b624b60e3cd1d
Rayssa Kailany <102701353+RayssaKailany@users.noreply.github.com>
2024-10-29 07:37:38 -0300

    ok

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,21 +1,21 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/romulopena/ciencia-de-dados/refs/heads/main/basededados/educacao-dados-globais.json&#39;
 
-console.log(url);
-
-async function vizualizarInformacoesGlobais() {
+async function visualizarInformacoesGlobais(){
     const res = await fetch(url)
     const dados = await res.json()
-    const paragrafo document.createElement('p')
+    const pessoasComAcesso = (dados.total_pessoas_com_acesso_a_educacao / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio_dia_estudando)
+    //const minutos = Math.round((dados.tempo_medio_dia_estudando - horas) * 100)
+    const pessoasEstudando = ((pessoasComAcesso / pessoasNoMundo) * 100).toFixed(2)
+    //console.log(dados)
+    const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
-  console.log(paragrafo)
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}</span> bilhões de pessoas e que aproximadamente <span>${pessoasEstudando}%</span> delas estão fazendo algum curso e passam em média <span>${horas} horas</span> estudando?`
+   
+   
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
 
-vizualizarInformacoesGlobais()
-
-paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente `
-
-// código omitido
-
-const container = document.getElementById('graficos-container')
-container.appendChild(paragrafo)
+visualizarInformacoesGlobais();
